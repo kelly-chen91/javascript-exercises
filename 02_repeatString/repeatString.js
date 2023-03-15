c972483f5afb5fe6baaff50b8e386b18f28f2421
Kelly Chen <kellychen966@gmail.com>
2023-03-15 19:00:14 -0400

    Fixed Repeat String

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, num_of_times) {
+    if(num_of_times < 0) return "ERROR";
+    let repeat_word = "";
+    for(let i = 0; i < num_of_times; i++){
+        repeat_word += word;
+    }
+    return repeat_word;
 };
 
 // Do not edit below this line
