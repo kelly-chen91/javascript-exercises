a96e04d04f35cd394f262dc67ad58c2dbc2c3e58
Kelly Chen <kellychen966@gmail.com>
2023-03-15 19:49:01 -0400

    fixed removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list, ...args) {
+    let remove_list = [];
+    let to_remove = false;
+    for(let i = 0; i < list.length; i++){
+        to_remove = false;
+        for(remove of args){ //loops through the args to match with the element. 
+            if(list[i] === remove){
+                to_remove = true; //it is better to have a boolean marker because if we were to append to the list right away, it will keep appending the same element
+            }
+        }
+        if(!to_remove) remove_list.push(list[i])
+    }
+    return remove_list;
 };
 
 // Do not edit below this line
