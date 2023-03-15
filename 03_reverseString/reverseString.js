dcf035ecfb0e2c384960a2c8b232d8cc0df5a58d
Kelly Chen <kellychen966@gmail.com>
2023-03-15 19:23:14 -0400

    fixed reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverse_word = "";
+    for(let i = word.length; i > 0; i--){
+        reverse_word += word.slice(i-1, i); //isolate substring of the word to add to reverse_word
+    }
+    return reverse_word;
 };
 
 // Do not edit below this line
