e776e8af14ac6dbc839f21b596e9758b62d425f6
Kelly Chen <kellychen966@gmail.com>
2023-03-15 22:15:08 -0400

    fixed leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    console.log("year % 4 = " + year % 4);
+    console.log("year % 100 = " + year % 100);
+    console.log("year % 400 = " + year % 400);
+    if(year % 4 == 0 && year % 100 !== 0) return true;
+    else if((year % 100 === 0) && year % 400 === 0) return true;
+    else return false;
+        
 };
 
 // Do not edit below this line
